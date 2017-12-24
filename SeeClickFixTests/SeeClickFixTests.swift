cdf81fd5a5be691c0ea42b87ed969bf55c03cce2
Doug Russell <doug@detroitblockworks.org>
2017-12-23 22:41:52 -0500

    tests

--- SeeClickFixTests/SeeClickFixTests.swift
+++ SeeClickFixTests/SeeClickFixTests.swift
@@ -10,16 +10,28 @@ import XCTest
 @testable import SeeClickFix
 
 class SeeClickFixTests: XCTestCase {
+    enum MockURLSessionError : Error {
+        case expectationMissing
+        case requestMismatch
+    }
     class MockURLSession : SCFURLSession {
-        var nextResult: SCFURLResult?
+        var expectedRequest: URLRequest?
+        var expectedResult: SCFURLResult?
+        func setURL(_ url: URL) {
+            expectedRequest = URLRequest(url: url)
+        }
         func scf_dataTaskPromise(with request: URLRequest) -> Promise<SCFURLResult> {
+            guard let expectedRequest = expectedRequest,
+                  let expectedResult = expectedResult else {
+                return Promise(error: MockURLSessionError.expectationMissing)
+            }
+            guard request == expectedRequest else {
+                print(request)
+                return Promise(error: MockURLSessionError.requestMismatch)
+            }
             let promise = Promise<SCFURLResult>()
-            if let result = nextResult {
-                DispatchQueue.main.asyncAfter(deadline: DispatchTime(uptimeNanoseconds: DispatchTime.now().uptimeNanoseconds + 100)) {
-                    promise.fulfill(result)
-                }
-            } else {
-                preconditionFailure()
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime(uptimeNanoseconds: DispatchTime.now().uptimeNanoseconds + 100)) {
+                promise.fulfill(expectedResult)
             }
             return promise
         }
@@ -36,14 +48,112 @@ class SeeClickFixTests: XCTestCase {
             return
         }
         let result = SCFURLResult(data: Data(), response: response)
-        SeeClickFixTests.mockSession.nextResult = result
-        let urlRequest = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 1.0)
-        let expectation = self.expectation(description: "testMockSession")
-        SeeClickFixTests.mockSession.scf_dataTaskPromise(with: urlRequest).then() { 
-            if result == $0 {
+        SeeClickFixTests.mockSession.expectedResult = result
+        let request = URLRequest(url: url)
+        SeeClickFixTests.mockSession.expectedRequest = request
+        let expectation = self.expectation(description: #function)
+        SeeClickFixTests.mockSession.scf_dataTaskPromise(with: request)
+            .then() {
+                if result == $0 {
+                    expectation.fulfill()
+                }
+            }
+            .catch() {
+                print($0)
+            }
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    func testLogin() {
+        guard let url = URL(string: "https://seeclickfix.com/api/v2/profile") else {
+            XCTFail()
+            return
+        }
+        guard let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil) else {
+            XCTFail()
+            return
+        }
+        var request = URLRequest(url: url)
+        request.setValue("Basic dGVzdDp0ZXN0", forHTTPHeaderField: "Authorization")
+        SeeClickFixTests.mockSession.expectedRequest = request
+        SeeClickFixTests.mockSession.expectedResult = SCFURLResult(data: Data(), response: response)
+        let expectation = self.expectation(description: #function)
+        SeeClickFixTests.mockNetwork.login(user: "test", password: "test")
+            .then() { _ in
                 expectation.fulfill()
             }
+            .catch() {
+                print($0)
+            }
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    func testIssues() {
+        guard let url = URL(string: "https://seeclickfix.com/api/v2/issues?lat=0.0&lng=0.0&page=0&per_page=100") else {
+            XCTFail()
+            return
         }
+        guard let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil) else {
+            XCTFail()
+            return
+        }
+        let result = SCFURLResult(data: Data(), response: response)
+        SeeClickFixTests.mockSession.setURL(url)
+        SeeClickFixTests.mockSession.expectedResult = result
+        let location = Point(latitude: 0.0, longitude: 0.0)
+        let expectation = self.expectation(description: #function)
+        SeeClickFixTests.mockNetwork.issues(location: location, page: 0, count: 100)
+            .then() {
+                if result.data == $0 {
+                    expectation.fulfill()
+                }
+            }
+            .catch() {
+                print($0)
+            }
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    func testReportTypes() {
+        guard let url = URL(string: "https://seeclickfix.com/api/v2/issues/new?lat=0.0&lng=0.0") else {
+            XCTFail()
+            return
+        }
+        guard let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil) else {
+            XCTFail()
+            return
+        }
+        let result = SCFURLResult(data: Data(), response: response)
+        SeeClickFixTests.mockSession.setURL(url)
+        SeeClickFixTests.mockSession.expectedResult = result
+        let location = Point(latitude: 0.0, longitude: 0.0)
+        let expectation = self.expectation(description: #function)
+        SeeClickFixTests.mockNetwork.reportTypes(location: location)
+            .then() { _ in
+                expectation.fulfill()
+            }
+            .catch() {
+                print($0)
+            }
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    func testReportDetails() {
+        guard let url = URL(string: "https://seeclickfix.com/api/v2/request_types/0") else {
+            XCTFail()
+            return
+        }
+        guard let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil) else {
+            XCTFail()
+            return
+        }
+        let result = SCFURLResult(data: Data(), response: response)
+        SeeClickFixTests.mockSession.setURL(url)
+        SeeClickFixTests.mockSession.expectedResult = result
+        let expectation = self.expectation(description: #function)
+        SeeClickFixTests.mockNetwork.reportDetails(identifier: 0)
+            .then() { _ in
+                expectation.fulfill()
+            }
+            .catch() {
+                print($0)
+            }
         waitForExpectations(timeout: 1.0, handler: nil)
     }
 }
